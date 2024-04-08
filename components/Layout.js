9301d606b1112d46d40a556edd5c03f97e8b56b7
Andrea Tardivo <an.tardivo@donboscosandona.it>
2024-04-08 10:48:43 +0200

    hgh

---
+++ components/Layout.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import CssBaseline from "@mui/material/CssBaseline"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import Container from '@mui/material/Container';
+import Header from "@/components/Header"
+
+const theme = createTheme();
+const sezioni= [
+    {titolo: "Informatico", url: "/informatico"},
+    {titolo: "Energie", url: "/energie"},
+    {titolo: "Elettrico", url: "/elettrico"},
+    {titolo: "Meccanico", url: "/meccanico"},
+    {titolo: "Motoristico", url: "/motoristico"},
+    {titolo: "Carrozzeria", url: "/carrozzeria"},
+    {titolo: "Trasparenza", url: "/Trasparenza"}
+]
+
+export default function Layout({children}){
+    return(
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Container maxWidth="lg">
+                <Header titolo="SFP DON BOSCO" sezioni={sezioni} />
+                <Head>
+                    <title>Componenti</title>
+                </Head>
+                <main>
+                    {children}
+                </main>
+            </Container>
+        </ThemeProvider>
+    )
+}
